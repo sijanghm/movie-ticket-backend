6d76a85ead2beb6a94b3dd80f772315d3599e564
sijanghm <sijan.ghimire29@gmail.com>
2023-07-18 09:15:01 +0545

    removed unused import

--- src/main/java/com/sijan/ticketbooking/security/JwtTokenProvider.java
+++ src/main/java/com/sijan/ticketbooking/security/JwtTokenProvider.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
